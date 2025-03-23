fc4ecf2b46d777c434daa27c19694227f7b2691b
Harsh-Koringa <22ucs088@lnmiit.ac.in>
2025-03-23 20:35:24 +0530

    header footer button and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
 import './App.css'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>{
+      setLoading(false)
+    })
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screan flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+    
+        <Header />
+      
+        <main>
+         TO DO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
 
 
-
-  return (
-    <>
-     <h1>A Blogging Platform</h1>
-    </>
-  )
+  ) : null
 }
 
 export default App
