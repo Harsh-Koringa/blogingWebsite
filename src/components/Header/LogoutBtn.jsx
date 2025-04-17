de38ea0bc86c8cc603b96817c84a410d0fb9ceba
Harsh-Koringa <22ucs088@lnmiit.ac.in>
2025-04-18 00:30:52 +0530

    debugging03

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -11,7 +11,8 @@ function LogoutBtn() {
         })
     }
   return (
-    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-200 rounded-full'>LogOut</button>
+    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-200 rounded-full'
+    onClick={logoutHandler}>LogOut</button>
   )
 }
 
