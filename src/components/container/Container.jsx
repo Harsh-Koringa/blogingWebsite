9fa0085053a45102541b6c80921caadbb27db88e
Harsh-Koringa <22ucs088@lnmiit.ac.in>
2025-07-15 00:55:47 +0530

    width manage

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({ children }) {
   return (
-    <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div className="mx-auto w-full px-4 sm:px-6">
       {children}
     </div>
   )
